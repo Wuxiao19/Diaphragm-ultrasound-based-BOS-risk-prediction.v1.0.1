ff49735e59e4f0effb7cf630e51ca3f93d537cc1
Wuxiao19 <wux3196737594@gmail.com>
2026-01-30 16:59:01 +0800

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -1,6 +1,20 @@
+import streamlit as st
+from random import randint
+from .session_state import get_session_state
 from streamlit import session_state as _state
 
 _PERSIST_STATE_KEY = f"{__name__}_PERSIST"
+
+
+
+state = get_session_state()
+if not state.widget_key:
+    state.widget_key = str(randint(1000, 100000000))
+uploaded_file = st.file_uploader(
+    "Choose a file", accept_multiple_files=True, key=state.widget_key)
+if st.button('clear uploaded_file'):
+    state.widget_key = str(randint(1000, 100000000))
+state.sync()
 
 
 def persist(key: str) -> str:
