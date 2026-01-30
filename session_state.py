720f5ebc0886ff48bd289d59028869274827b466
Wuxiao19 <wux3196737594@gmail.com>
2026-01-30 17:01:28 +0800

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from random import randint
 from .session_state import get_session_state
-from streamlit import session_state as _state
-
-_PERSIST_STATE_KEY = f"{__name__}_PERSIST"
-
-
 
 state = get_session_state()
 if not state.widget_key:
@@ -15,25 +10,3 @@ uploaded_file = st.file_uploader(
 if st.button('clear uploaded_file'):
     state.widget_key = str(randint(1000, 100000000))
 state.sync()
-
-
-def persist(key: str) -> str:
-    """Mark widget state as persistent."""
-    if _PERSIST_STATE_KEY not in _state:
-        _state[_PERSIST_STATE_KEY] = set()
-
-    _state[_PERSIST_STATE_KEY].add(key)
-
-    return key
-
-
-def load_widget_state():
-    """Load persistent widget state."""
-    if _PERSIST_STATE_KEY in _state:
-        _state.update(
-            {
-                key: value
-                for key, value in _state.items()
-                if key in _state[_PERSIST_STATE_KEY]
-            }
-        )
